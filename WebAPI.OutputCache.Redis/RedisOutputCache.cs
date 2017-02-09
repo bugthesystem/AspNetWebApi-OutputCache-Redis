386ded298b4124fd8a19d3741f03b7e36d0bcffa
Ziya SARIKAYA <ziya.sarikaya@here.com>
2017-02-09 19:29:03 +0100

    closes #1

--- WebAPI.OutputCache.Redis/RedisOutputCache.cs
+++ WebAPI.OutputCache.Redis/RedisOutputCache.cs
@@ -11,18 +11,19 @@ namespace WebAPI.OutputCache.Redis
     {
         private readonly IJsonSerializer _jsonSerializer;
         private readonly IRedisConnectionSettings _connectionSettings;
-        private readonly IConnectionMultiplexer _multiplexer;
+        private readonly Lazy<IConnectionMultiplexer> _multiplexer;
         readonly Options _options;
 
         public RedisOutputCache(IJsonSerializer jsonSerializer, IRedisConnectionSettings connectionSettings)
         {
             _jsonSerializer = jsonSerializer;
             _connectionSettings = connectionSettings;
-            _multiplexer = ConnectionMultiplexer.Connect(_connectionSettings.ConnectionString);
+            _multiplexer = new Lazy<IConnectionMultiplexer>(() => ConnectionMultiplexer.Connect(_connectionSettings.ConnectionString));
+
             _options = null;//TODO:
         }
 
-        private IDatabase DB => _multiplexer.GetDatabase(_connectionSettings.Db);
+        private IDatabase Db => _multiplexer.Value.GetDatabase(_connectionSettings.Db);
 
         public void RemoveStartsWith(string key)
         {
@@ -30,12 +31,12 @@ namespace WebAPI.OutputCache.Redis
             //TODO: delete by pattern 
             //EVAL "return redis.call('del', unpack(redis.call('keys', ARGV[1])))" 0 prefix:*
             //CAUTION : while deleting keys please use scan 
-            DB.KeyDelete(key);
+            Db.KeyDelete(key);
         }
 
         public T Get<T>(string key) where T : class
         {
-            string redisValue = DB.StringGet(key);
+            string redisValue = Db.StringGet(key);
 
             if (!string.IsNullOrEmpty(redisValue))
                 return _jsonSerializer.DeserializeObject<T>(redisValue, _options);
@@ -45,23 +46,23 @@ namespace WebAPI.OutputCache.Redis
 
         public object Get(string key)
         {
-            return DB.StringGet(key);
+            return Db.StringGet(key);
         }
 
         public void Remove(string key)
         {
-            DB.KeyDelete(key);
+            Db.KeyDelete(key);
         }
 
         public bool Contains(string key)
         {
-            return DB.KeyExists(key);
+            return Db.KeyExists(key);
         }
 
         public void Add(string key, object o, DateTimeOffset expiration, string dependsOnKey = null)
         {
             TimeSpan timeSpan = expiration.DateTime.Subtract(DateTime.Now);
-            DB.StringSet(key, _jsonSerializer.SerializeObject(o, _options), timeSpan);
+            Db.StringSet(key, _jsonSerializer.SerializeObject(o, _options), timeSpan);
         }
 
         //TODO: use SCAN to get keys
